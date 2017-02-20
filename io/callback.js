137781f88fbf8a06fc5ce0e37a8e7a47348041a0
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2017-02-20 22:08:29 +0100

    [CHAPTER3] get http callback module

--- io/callback.js
+++ io/callback.js
@@ -1,8 +1,8 @@
 'use strict';
 
-/*
-node utiliza los callbacks para resolver el modelo de IO (las interfaces que permiten resolver el modelo de concurrencia)
-*/
+/**
+ * node use callbacks to solve IO model (all the interfaces that allow solve concurrent IO model)
+ */
 
 let fs = require('fs');
 
@@ -10,3 +10,19 @@ fs.readFile('./data/callback-test.txt', 'utf-8', function (err, data) {
     if (err) throw err;
     console.info(data);
 });
+
+/**
+ * Callback that resolves the http module
+ * in this situation we are not able to predicit the order of the response on each script execution
+ */
+let http = require('http');
+
+http.get({host: 'shapeshed.com'}, function (res) {
+    console.info(`Get code response fom shapeshed: ${res.statusCode}`);
+
+}).on('error', console.error);
+
+http.get({host: 'unknouwn.not'}, console.info)
+    .on('error', function (err) {
+        console.error(`Got error: ${err.message}`);
+    });
